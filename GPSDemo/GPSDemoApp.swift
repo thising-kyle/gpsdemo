a3edeadd7dd959d12123133d7fc85a82c5db8572
Old K <45676439+thising-kyle@users.noreply.github.com>
2022-11-29 16:44:37 +0800

    Initial Commit

---
+++ GPSDemo/GPSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSDemoApp.swift
+//  GPSDemo
+//
+//  Created by Kyle Davis on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct GPSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
